e294a5cfe1286c52eca6e7db2c83ec6ce252207d
ChrisBartholomew20 <155928081+ChrisBartholomew20@users.noreply.github.com>
2025-04-13 10:48:55 -0400

    Add files via upload
    Code that searches through the first 10 million digits of pi for a string of numbers that is inputted by the user

---
+++ TranscendYourself.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+
+
+
+
+
+int main() {
+    string search_sequence;
+    ifstream file("Pi-10.txt"); 
+    string pi;
+
+    if (file.is_open()) {
+        getline(file, pi, '\0');
+        file.close();
+    }
+    else {
+        std::cerr << "Unable to open file!" << std::endl;
+        return 1;
+    }
+
+    cout << "What sequence do you want to search for? " << endl;
+        cin >> search_sequence;
+
+        //My birthday is 072403, found at 758052
+       
+  
+    size_t position = pi.find(search_sequence);
+
+    if (position != string::npos) {
+         
+        cout << "Sequence " << search_sequence << " found at position: " << position << endl;
+    }
+    else {
+      
+       cout << "Sequence " << search_sequence << " not found in π." << endl;
+    }
+
+   
+
+    
+
+    return 0;
+}
